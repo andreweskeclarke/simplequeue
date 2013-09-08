a49f82b0e55e43e7a3b539833fdb5f22edf449c0
andreweskeclarke <andreweskeclarke@gmail.com>
2013-09-08 12:51:28 -0500

    Creating a message listener in jms

--- simplejms/src/main/java/com/aeclarke/activemqsample/App.java
+++ simplejms/src/main/java/com/aeclarke/activemqsample/App.java
@@ -1,5 +1,9 @@
 package com.aeclarke.activemqsample;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Starting...");
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("http://localhost");
+        try {
+            Connection connection = connectionFactory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            session.setMessageListener(new SimpleMessageListener());
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class SimpleMessageListener implements MessageListener {
+        @Override
+        public void onMessage(Message message) {
+            try {
+                String text = ((TextMessage) message).getText();
+                System.out.println(text);
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
