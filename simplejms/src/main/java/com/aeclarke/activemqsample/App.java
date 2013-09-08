3b91e7d3b093651cb7b5c145ab01119ac7bfca28
andreweskeclarke <andreweskeclarke@gmail.com>
2013-09-08 13:30:13 -0500

    Listener is listening to localhost

--- simplejms/src/main/java/com/aeclarke/activemqsample/App.java
+++ simplejms/src/main/java/com/aeclarke/activemqsample/App.java
@@ -1,26 +1,41 @@
 package com.aeclarke.activemqsample;
 
+import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.util.Calendar;
+import java.util.Date;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+
+    public static final String BROKER_URL = ActiveMQConnection.DEFAULT_BROKER_URL;
+    public static final String TOPIC_NAME = "SIMPLE.TOPIC";
+    public static final String SUBSCRIBER_NAME = "SimpleJMS";
+
     public static void main( String[] args )
     {
         System.out.println("Starting...");
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("http://localhost");
         try {
-            Connection connection = connectionFactory.createConnection();
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            session.setMessageListener(new SimpleMessageListener());
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(BROKER_URL);
+            TopicConnection connection = connectionFactory.createTopicConnection();
+            connection.setClientID(getClientId());
+            TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic topic =  session.createTopic(TOPIC_NAME);
+            TopicSubscriber subscriber = session.createDurableSubscriber(topic, SUBSCRIBER_NAME);
+            subscriber.setMessageListener(new SimpleMessageListener());
+            connection.start();
         } catch (JMSException e) {
+            System.out.println("Failed!");
             e.printStackTrace();
+            return;
         }
+        System.out.println("Started listening!");
+    }
+
+    private static String getClientId() {
+        return "com.aeclarke.activemqsample" + Calendar.getInstance().getTimeInMillis();
     }
 
     private static class SimpleMessageListener implements MessageListener {
