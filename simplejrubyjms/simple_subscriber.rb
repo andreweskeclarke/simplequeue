ab147070cc00a056d71f93f15f72c33f509b1853
andreweskeclarke <andreweskeclarke@gmail.com>
2013-09-08 17:34:06 -0500

    Changing JRuby JMS approach

--- simplejrubyjms/simple_subscriber.rb
+++ simplejrubyjms/simple_subscriber.rb
@@ -1,27 +1,46 @@
-require 'rubygems'
-require 'jms'
+require "java"
+require "~/Programming/apache-activemq-5.5.1/activemq-all-5.5.1.jar"
+require "~/Programming/apache-activemq-5.5.1/lib/optional/log4j-1.2.14.jar"
+require "~/Programming/apache-activemq-5.5.1/lib/optional/slf4j-log4j12-1.5.11.jar"
 
-config = {
-  :factory => 'org.apache.activemq.ActiveMQConnectionFactory',
-  :broker_url => 'tcp://localhost:61616',
-  :require_jars => [
-    "~/Programming/apache-activemq-5.5.1/activemq-all-5.5.1.jar",
-    "~/Programming/apache-activemq-5.5.1/lib/optional/log4j-1.2.14.jar",
-    "~/Programming/apache-activemq-5.5.1/lib/optional/slf4j-log4j12-1.5.11.jar"
-  ]
-}
+include_class "org.apache.activemq.ActiveMQConnectionFactory"
+include_class "org.apache.activemq.util.ByteSequence"
+include_class "org.apache.activemq.command.ActiveMQBytesMessage"
+include_class "javax.jms.MessageListener"
+include_class "javax.jms.Session"
 
-connection = JMS::Connection.new(config)
+class MessageHandler
+  include javax.jms.Session
+  include javax.jms.MessageListener
 
-connection.on_message(:topic_name => 'SIMPLE.TOPIC', :options => JMS::Session::AUTO_ACKNOWLEDGE) do |message|
-  begin
-    p message
-  rescue
-    return false
+  def onMessage(serialized_message)
+    message_body = serialized_message.get_content.get_data.inject("") { |body, byte| body << byte }
+    puts message_body
   end
-  true
+
+  def run
+    factory = ActiveMQConnectionFactory.new("tcp://localhost:61616")
+    connection = factory.create_connection();
+    connection.set_client_id("JRUBY" + Time.now.to_i.to_s);
+    session = connection.create_session(false, Session::AUTO_ACKNOWLEDGE);
+    topic = session.create_topic("SIMPLE.TOPIC");
+
+    subscriber = session.create_durable_subscriber(topic, "JRUBY");
+    subscriber.set_message_listener(self);
+
+    connection.start();
+    puts "Listening..."
+  end
 end
 
+handler = MessageHandler.new
+handler.run
 
-while true do
-end
+# class EventsProcessor < ApplicationProcessor
+#
+#   subscribes_to :events
+#   def on_message(message)
+#     ...do stuff....
+#   end
+# end
+#
